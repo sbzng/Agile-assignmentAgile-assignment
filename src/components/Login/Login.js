31aac254a8fc6f3946d01b8eb85f611ce0e607f4
Shaobo Zang <20105527@mail.wit.ie>
2023-11-26 22:49:27 +0000

    search movie

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,84 +7,46 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import "./Login.css";
-
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
-
   useEffect(() => {
     if (loading) {
+
       return;
     }
-    if (user) {
-      navigate("/");
-    }
+    if (user) navigate("/");
   }, [user, loading]);
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    logInWithEmailAndPassword(email, password);
-  };
-
   return (
     <div className="login">
       <Card sx={{ maxWidth: 345 }}>
         <Typography gutterBottom variant="h5" component="div" className="cardTitle">
           TMDB Client
         </Typography>
-        <form className="form" onSubmit={handleLogin}>
-          <div>
-            <TextField
-              type="text"
-              className="login__textBox"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email Address"
-              id="login-email"
-              label="Email address"
-              variant="outlined"
-              aria-label="Email address"
-            />
-          </div>
-          <div>
-            <TextField
-              type="password"
-              className="login__textBox"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              id="login-password"
-              label="Password"
-              variant="outlined"
-              aria-label="Password"
-            />
-          </div>
-          <div>
-            <Button
-              className="login__btn"
-              type="submit"
-              id="login_button"
-              variant="outlined"
-              aria-label="Login"
-            >
-              Login
-            </Button>
-          </div>
-          <div>
-            <Button
-              className="login__btn login__google"
-              onClick={signInWithGoogle}
-              variant="outlined"
-              aria-label="Login with Google"
-            >
-              Login with Google
-            </Button>
-          </div>
+        <form className="form">
+          <TextField type="text"
+            className="login__textBox"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email Address" id="login-email" label="Email address" variant="standard" />
+          <br />
+          <TextField type="password"
+            className="login__textBox"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password" id="login-password" label="Password" variant="standard" />
+          <br />
+          <Button className="login__btn"
+            onClick={() => logInWithEmailAndPassword(email, password)} id ="login_button" variant="outlined">Login</Button>
+          <br />
+          <Button className="login__btn login__google" onClick={signInWithGoogle} variant="outlined">Login with Google</Button>
+          <br />
           <Typography variant="p" color="text.secondary" className="forgotPassword">
             <Link to="/reset">Forgot Password</Link>
           </Typography>
+          <br />
           <Typography variant="p" color="text.secondary">
             Don't have an account? <Link to="/register">Register</Link> now.
           </Typography>
@@ -93,5 +55,4 @@ function Login() {
     </div>
   );
 }
-
 export default Login;
